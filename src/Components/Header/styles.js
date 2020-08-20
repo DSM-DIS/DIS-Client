71ff2e85e59cdcdf8e713d28625d84f60a8436ea
CutyApple <dbtldhs0915@naver.com>
2020-08-20 21:59:35 +0900

    [ADD] <Header.js> Header UI

--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -2,4 +2,39 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	display: flex;
+	width: 100%;
+	height: 80px;
+	background: white;
+	justify-content: center;
+`;
+
+export const HeaderLogo = styled.div`
+	display: flex;
+	position: absolute;
+	left: 24px;
+	width: 123px;
+	height: 56px;
+	color: #f76d6d;
+	font-size: 50px;
+	font-weight: bold;
+`;
+
+export const HeaderButtonWrap = styled.div`
+	display: flex;
+	position: absolute;
+	right: 66px;
+`;
+
+export const HeaderButton = styled.button`
+	display: flex;
+	width: 129px;
+	height: 49px;
+	color: #ff9d9d;
+	font-size: 20px;
+	font-weight: bold;
+	border: 4px solid #ff9d9d;
+	border-radius: 49px;
+	background: white;
+	align-content: center;
+	justify-content: center;
 `;
