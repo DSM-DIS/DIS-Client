dabfbccfb254dcd6b31380558768547e080bac89
CutyApple <dbtldhs0915@naver.com>
2020-08-21 09:59:11 +0900

    [FIX] <Header.js> Header UI

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import * as S from './styles';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 const Main = () => {
-	return <S.Container></S.Container>;
+	return (
+		<S.Container>
+			<Header />
+			<Footer />
+		</S.Container>
+	);
 };
 
 export default Main;
