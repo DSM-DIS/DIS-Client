234105f9bb0f5f5fa53ea5dfbe2b7a5e35192222
CutyApple <dbtldhs0915@naver.com>
2020-08-21 11:40:41 +0900

    [FIX] <Main.js> CSS 변경

--- src/Components/Main/DirayContainer/styles.js
+++ src/Components/Main/DirayContainer/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
 	display: flex;
 	width: calc(100% - 190px);
-	padding: 95px;
+	padding: 95px 95px 60px 95px;
 	background: #17202e;
 	flex-wrap: wrap;
 	justify-content: space-between;
