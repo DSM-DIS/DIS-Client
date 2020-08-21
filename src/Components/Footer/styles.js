e2c7f42faba771412f3bb5f05f5040a684a4cc2e
CutyApple <dbtldhs0915@naver.com>
2020-08-21 09:40:26 +0900

    [ADD] <Footer.js> Footer UI

--- src/Components/Footer/styles.js
+++ src/Components/Footer/styles.js
@@ -2,4 +2,31 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	display: flex;
+	width: 100%;
+	height: 327px;
+	background: #2e2e2e;
+	flex-direction: column;
+`;
+
+export const MainAnchorWrap = styled.div`
+	display: flex;
+`;
+
+export const MainAnchor = styled.div`
+	display: flex;
+	color: white;
+	font-size: 30px;
+	font-weight: bold;
+`;
+
+export const SubAnchorWrap = styled(MainAnchorWrap)``;
+
+export const SubAnchor = styled(MainAnchor)`
+	color: #a8a8a8;
+	font-size: 25px;
+	font-weight: 300;
+`;
+
+export const SubText = styled(SubAnchor)`
+	font-size: 20px;
 `;
