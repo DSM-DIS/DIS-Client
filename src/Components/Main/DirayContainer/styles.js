e14fb41a78a9a950d108969e661f00a5f4873198
CutyApple <dbtldhs0915@naver.com>
2020-08-21 10:52:13 +0900

    [CREATE] <DirayContainer.js> File init

---
+++ src/Components/Main/DirayContainer/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	display: flex;
+	width: calc(100% - 190px);
+	padding: 95px;
+	background: #17202e;
+	flex-wrap: wrap;
+`;
