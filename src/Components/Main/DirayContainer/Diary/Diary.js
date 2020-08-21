080d4e5e8cfa79aed66ebcb06ffce80bc8580dc5
CutyApple <dbtldhs0915@naver.com>
2020-08-21 11:37:40 +0900

    [ADD] <Diary.js> 다이어리 UI 구현

---
+++ src/Components/Main/DirayContainer/Diary/Diary.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as S from './styles';
+
+const Diary = (props) => {
+	const { mode } = props;
+
+	console.log(mode);
+
+	return (
+		<S.Container>
+			{mode === 'init' ? (
+				<>
+					<S.DiaryShape mode={mode} />
+					<S.DiaryText>{`일기 생성하기`}</S.DiaryText>
+				</>
+			) : (
+				<>
+					<S.DiaryShape>{`DIARY`}</S.DiaryShape>
+					<S.DiaryText>{`Temp Text`}</S.DiaryText>
+				</>
+			)}
+		</S.Container>
+	);
+};
+
+export default Diary;
