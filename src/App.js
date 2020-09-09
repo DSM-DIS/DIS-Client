824951584dec0ff602d8d47a5dda05d877fe9ac1
Sonwanseo <sonwanseo@gmail.com>
2020-09-09 18:59:28 +0900

    [CREATE] Login Page Publishing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Routing from './Routing';
-import { GlobalStyle } from './GlobalStyle';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routing from "./Routing";
+import { GlobalStyle } from "./GlobalStyle";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import LoginContainer from "./Containers/Login/LoginContainer";
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<GlobalStyle />
-			<Routing />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routing />
+      <Switch>
+        <Route path="/login" component={LoginContainer} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/Components/Login/Share/Share.js
@@ -0,0 +1,28 @@
+import React from "react";
+import * as S from "./style";
+import WriteDiv from "../../common/WriteDiv/WriteDiv";
+import NumberBox from "./NumberBox/NumberBox";
+import InputBox from "./InputBox/InputBox";
+
+const slogan = "나 혼자가 아닌 다른 사람들과!";
+const title = "코드를 사용하여 공유 가능한 일기";
+const explanation =
+  "코드 번호로 언제든 다른 사람과 나의 일기장을 공유할 수 있습니다. 다른 누군가와 함께 일기를 공유해보세요!";
+
+const Share = () => {
+  return (
+    <S.ShareWrapper>
+      <S.ShareLeftWrapper>
+        <S.ShareWriteDiv>
+          <WriteDiv slogan={slogan} title={title} explanation={explanation} />
+        </S.ShareWriteDiv>
+      </S.ShareLeftWrapper>
+      <S.ShareRightWrapper>
+        <NumberBox />
+        <InputBox />
+      </S.ShareRightWrapper>
+    </S.ShareWrapper>
+  );
+};
+
+export default Share;
