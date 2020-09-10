43a17545fb339ed429224083743d1ba2af219889
Giljun Bae <bgj0127@gmail.com>
2020-09-10 20:18:26 +0900

    [FIX] <App.js> 라우팅
    writing 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Routing from './Routing';
-import { GlobalStyle } from './GlobalStyle';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Routing from "./Routing";
+import { GlobalStyle } from "./GlobalStyle";
+import WritingContainer from "./Containers/Writing/WritingContainer";
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<GlobalStyle />
-			<Routing />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routing />
+      <Switch>
+        <Route path="/writing" component={WritingContainer} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
