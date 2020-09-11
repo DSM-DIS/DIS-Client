feadca0b0a58de20886f346fb414f8e28e6c51d3
Giljun Bae <bgj0127@gmail.com>
2020-09-11 15:57:56 +0900

    [REMOVE] 커밋 오류 삭제

--- src/Containers/Login/Footer/FooterContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Footer from "../../../Components/Login/Footer/Footer";
-
-const FooterContainer = () => {
-  return <Footer />;
-};
-
-export default FooterContainer;
--- src/Containers/Login/Register/RegisterContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Register from "../../../Components/Login/Register/Register";
-
-const RegisterContainer = () => {
-  return <Register />;
-};
-
-export default RegisterContainer;
