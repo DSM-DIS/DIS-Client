d1d394db2bf88073fc2f40a2544c34408dc98a9a
Giljun Bae <bgj0127@gmail.com>
2020-09-11 11:09:21 +0900

    Auto stash before merge of "feature/writing" and "origin/feature/writing"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+<<<<<<< Updated upstream
 import { BrowserRouter } from "react-router-dom";
 import Routing from "./Routing";
 import { GlobalStyle } from "./GlobalStyle";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LoginContainer from "./Containers/Login/LoginContainer";
+=======
+
+>>>>>>> Stashed changes
 import WritingContainer from "./Containers/Writing/WritingContainer";
 
 const App = () => {
