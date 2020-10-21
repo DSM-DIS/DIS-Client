ab5e2f7162d6aba6c7349654ea5018b3000e4de2
CutyApple <dbtldhs0915@naver.com>
2020-10-21 22:16:45 +0900

    [CREATE] <MainContiner.js> File init

---
+++ src/Containers/Main/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../../Components/Main/Main';
+
+const MainContainer = () => {
+	return (
+		<>
+			<Main />
+		</>
+	);
+};
+
+export default MainContainer;
