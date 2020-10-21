75bc03b0a31ce7e31852facdd3335e512bc3a9d8
CutyApple <dbtldhs0915@naver.com>
2020-10-21 22:16:14 +0900

    [MODIFY] <src/Routing.js> Component -> Container 변환

--- src/Routing.js
+++ src/Routing.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Main from './Components/Main/Main';
 import LoginContainer from './Containers/Login/LoginContainer';
+import MainContainer from './Containers/Main/MainContainer';
 import WritingContainer from './Containers/Writing/WritingContainer';
 
 const Routing = () => {
 	return (
 		<Switch>
 			<Route exact path="/" component={LoginContainer} />
-			<Route path="/main" component={Main} />
+			<Route path="/main" component={MainContainer} />
 			<Route path="/write" component={WritingContainer} />
 		</Switch>
 	);
