4861b2ce1e9556cc13e3effccdeb6776ffe02dee
CutyApple <dbtldhs0915@naver.com>
2020-10-21 22:50:04 +0900

    [CREATE] <Main/Drawer.js> File init

---
+++ src/Components/Main/Drawer/Drawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as S from './styles.js';
+
+const Drawer = () => {
+	return (
+		<S.MainWrap>
+			<S.MainHeader>
+				<S.MainHeaderText>
+					<S.UserName>{`아이유`}</S.UserName>님의 서랍장
+				</S.MainHeaderText>
+			</S.MainHeader>
+			<DiaryWrap />
+		</S.MainWrap>
+	);
+};
+
+export default Drawer;
