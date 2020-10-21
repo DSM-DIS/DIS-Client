12c7beb53bec223f92f77b13eb33611bdc37b9c7
CutyApple <dbtldhs0915@naver.com>
2020-10-21 22:49:36 +0900

    [CREATE] <Main/DrawerContainer.js> File init

---
+++ src/Containers/Main/DrawerContainer/DrawerContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Drawer from '../../../Components/Main/Drawer/Drawer';
+
+const DrawerContainer = () => {
+	return <Drwaer />;
+};
+
+export default DrawerContainer;
