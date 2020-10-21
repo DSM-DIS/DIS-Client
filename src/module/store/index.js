5cc09e0db498f65d6336158603c2e8183096294b
CutyApple <dbtldhs0915@naver.com>
2020-10-21 22:10:41 +0900

    [ADD] <store/index.js> default setting

--- src/module/store/index.js
+++ src/module/store/index.js
@@ -1,11 +1,16 @@
 import createSaga from 'redux-saga';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import rootSaga from '../saga';
+import rootReducer from '../reducer';
 
 const saga = createSaga();
 
-const saga = createStore(composeWithDevTools(applyMiddleware(saga)));
+const saga = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(saga)),
+);
 
-saga.run();
+saga.run(rootSaga);
 
 export default StoreExceptionsInformation;
