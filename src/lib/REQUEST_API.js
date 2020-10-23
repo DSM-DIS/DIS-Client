8e9c027d80a5b9155e8e377726fa70f2561b1517
CutyApple <dbtldhs0915@naver.com>
2020-10-23 16:28:18 +0900

    [CREATE] <lib/REQUEST_API.js> File init

---
+++ src/lib/REQUEST_API.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'https://DIaryStory.com';
+
+const ACCESS_TOKEN_NAME = 'Authorization';
+const ACCESS_TOKEN = 'accessToken';
+const REFRESH_TOKEN = 'refreshToken';
+
+export const methodType = {
+	GET: 'get',
+	POST: 'post',
+	PUT: 'put',
+	PATCH: 'patch',
+};
