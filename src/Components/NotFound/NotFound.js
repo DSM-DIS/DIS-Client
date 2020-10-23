612ef41f8139624aa76bc6ce154da78e412ba3bf
CutyApple <dbtldhs0915@naver.com>
2020-10-23 16:02:03 +0900

    [ADD] <Components/NotFound.js> Style 추가

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -2,7 +2,14 @@ import React from 'react';
 import * as S from './styles';
 
 const NotFound = () => {
-	return <S.Container></S.Container>;
+	return (
+		<S.Container>
+			<S.ErrorWrap>
+				<S.MainText>404</S.MainText>
+				<S.SubText>Page Not Found</S.SubText>
+			</S.ErrorWrap>
+		</S.Container>
+	);
 };
 
 export default NotFound;
--- src/Components/NotFound/styles.js
+++ src/Components/NotFound/styles.js
@@ -2,5 +2,45 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	display: flex;
-	background: white;
+	height: 100%;
+	background: #ffffff;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const ErrorWrap = styled.div`
+	position: relative;
+	display: flex;
+	width: 540px;
+	height: 340px;
+	border: 5px solid black;
+	align-items: center;
+	justify-content: space-evenly;
+	flex-direction: column;
+
+	&::after {
+		position: absolute;
+		display: flex;
+		content: 'error';
+		width: 200px;
+		height: 50px;
+		font-size: 54px;
+		background: #ffffff;
+		align-items: flex-end;
+		justify-content: center;
+		top: 0;
+		transform: translateY(-50%);
+	}
+`;
+
+export const MainText = styled.p`
+	color: #000000;
+	font-size: 130px;
+	font-weight: bold;
+`;
+
+export const SubText = styled.p`
+	color: #000000;
+	font-size: 30px;
+	font-weight: bold;
 `;
