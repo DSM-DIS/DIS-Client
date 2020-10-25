551dd02d5735cc9532dba3a3a593c4140968be51
CutyApple <dbtldhs0915@naver.com>
2020-10-25 16:45:21 +0900

    [ADD] <module/diary_list> ADD DUCKS SAGA ACTION

--- src/module/diary_list/index.js
+++ src/module/diary_list/index.js
@@ -1,5 +1,6 @@
-export const SET_DIARY_BOOK_LIST = 'diary_list/SET_DIARY_BOOK_LIST';
+const SET_DIARY_BOOK_LIST = 'diary_list/SET_DIARY_BOOK_LIST';
 
+export const GET_DIARY_BOOK_LIST_SAGA = 'diary_list/GET_DIARY_BOOK_LIST_SAGA';
 
 const initialState = {
     diaryList = [],
