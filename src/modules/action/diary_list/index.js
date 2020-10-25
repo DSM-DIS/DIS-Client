a3033b3541de380e1d6ac5394a51fc7721bc85ed
CutyApple <dbtldhs0915@naver.com>
2020-10-25 17:11:51 +0900

    [DELETE] <modules/action> DUCKS PATTERN

--- src/modules/action/diary_list/index.js
+++
@@ -1,8 +0,0 @@
-export const SET_DIARY_BOOK_LIST = 'diary_list/SET_DIARY_BOOK_LIST';
-
-export const GET_DIARY_BOOK_LIST_SAGA = 'diary_list/GET_DIARY_BOOK_LIST_SAGA';
-
-export const setDiaryBookList = (payload) => ({
-	type: SET_DIARY_BOOK_LIST,
-	payload,
-});
