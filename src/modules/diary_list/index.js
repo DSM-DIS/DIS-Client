e1a93627972f795dc3865723440312cfc6b06c5f
CutyApple <dbtldhs0915@naver.com>
2020-10-25 17:05:25 +0900

    [MODIFY] <modules> Naming Convention

--- src/modules/diary_list/index.js
+++ src/modules/diary_list/index.js
@@ -2,6 +2,13 @@ const SET_DIARY_BOOK_LIST = 'diary_list/SET_DIARY_BOOK_LIST';
 
 export const GET_DIARY_BOOK_LIST_SAGA = 'diary_list/GET_DIARY_BOOK_LIST_SAGA';
 
+
+export const setDiaryBookList = (payload) => ({
+	type: SET_DIARY_BOOK_LIST,
+	payload,
+});
+
+
 const initialState = {
     diaryList = [],
 }
