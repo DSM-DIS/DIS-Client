cf63e6679ffc81236f063a954d1f2effa2ce9deb
CutyApple <dbtldhs0915@naver.com>
2020-11-06 19:38:05 +0900

    [FEAT] <src/Routing.js> ModalWrapper 라우팅 추가

--- src/Routing.js
+++ src/Routing.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import LoginContainer from './Containers/Login/LoginContainer';
-import MainContainer from './Containers/Main/MainContainer';
-import NotFoundContainer from './Containers/NotFound/NotFoundContainer';
-import WritingContainer from './Containers/Writing/WritingContainer';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import ModalWrapperContainer from "./containers/ModalWrapper/ModalWrapperContainer";
+import LoginContainer from "./containers/Login/LoginContainer";
+import MainContainer from "./containers/Main/MainContainer";
+import NotFoundContainer from "./containers/NotFound/NotFoundContainer";
+import WritingContainer from "./containers/Writing/WritingContainer";
 
 const Routing = () => {
 	return (
-		<Switch>
-			<Route exact path="/" component={LoginContainer} />
-			<Route path="/main" component={MainContainer} />
-			<Route path="/write" component={WritingContainer} />
-			<Route component={NotFoundContainer} />
-		</Switch>
+		<>
+			<Switch>
+				<Route exact path="/" component={LoginContainer} />
+				<Route path="/main" component={MainContainer} />
+				<Route path="/write" component={WritingContainer} />
+				<Route component={NotFoundContainer} />
+			</Switch>
+			<ModalWrapperContainer />
+		</>
 	);
 };
 
