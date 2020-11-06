c2dd371b7327fef2b890f6611414dc91aff5a75a
CutyApple <dbtldhs0915@naver.com>
2020-11-06 16:56:46 +0900

    [FEAT] <containers/ModalWrapperContainer> ModalWrapperContainer 컨테이너 추가

---
+++ src/containers/ModalWrapper/ModalWrapperContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ModalWrapper from "../../components/modal/ModalWrapper/ModalWrapper";
+
+const ModalWrapperContainer = () => {
+	return <ModalWrapper />;
+};
+
+export default ModalWrapperContainer;
