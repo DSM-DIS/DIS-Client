50cca18e2dbb89282c2dedacad0a1e608d1b8f9e
CutyApple <dbtldhs0915@naver.com>
2020-11-07 22:20:38 +0900

    [FEAT] <components/DiaryModal.js> DiaryModal 기능 완성

--- src/components/modal/modal/diaryModal/DiaryModal.js
+++ src/components/modal/modal/diaryModal/DiaryModal.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Modal from "../../modal/Modal";
+import ModalHeader from "../../modalHeader/ModalHeader";
+import ModalCloseBtn from "../../modalCloseBtn/ModalCloseBtn";
+import DiaryModalFooter from "../../ModalFooter/DiaryModalFooter/DiaryModalFooter";
 
 const DiaryModal = () => {
-	return <div></div>;
+	return (
+		<Modal>
+			<ModalCloseBtn />
+			<ModalHeader text={"일기장생성"} />
+			<DiaryModalFooter />
+		</Modal>
+	);
 };
 
 export default DiaryModal;
