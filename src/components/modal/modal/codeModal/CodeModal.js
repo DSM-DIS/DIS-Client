d54c1d879c9268e52b9312f201ce2f8df530b25b
CutyApple <dbtldhs0915@naver.com>
2020-11-07 23:46:41 +0900

    [FEAT] <components/CodeModal.js> Form 추가

--- src/components/modal/modal/codeModal/CodeModal.js
+++ src/components/modal/modal/codeModal/CodeModal.js
@@ -2,12 +2,14 @@ import React from "react";
 import Modal from "../Modal";
 import ModalHeader from "../../modalHeader/ModalHeader";
 import ModalCloseBtn from "../../ModalCloseBtn/ModalCloseBtn";
+import CodeModalForm from "../../ModalForm/CodeModalForm/CodeModalForm";
 
 const CodeModal = () => {
 	return (
 		<Modal>
 			<ModalCloseBtn />
 			<ModalHeader text={"코드입력"} />
+			<CodeModalForm />
 		</Modal>
 	);
 };
