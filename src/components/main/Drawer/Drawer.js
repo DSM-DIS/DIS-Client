469c579eaab431ee31bda9d2083dad86fd24bded
CutyApple <dbtldhs0915@naver.com>
2020-11-07 23:47:42 +0900

    [TEST] <components/Drawer.js> Testing

--- src/components/main/Drawer/Drawer.js
+++ src/components/main/Drawer/Drawer.js
@@ -3,12 +3,13 @@ import * as S from "./styles.js";
 import DiaryWrap from "./DirayWrap/DirayWrap";
 import { useDispatch } from "react-redux";
 import { showModal } from "../../../modules/redux/modal/index.js";
+import CodeModal from "../../modal/Modal/Codemodal/CodeModal";
 import DiaryModal from "../../modal/Modal/DiaryModal/DiaryModal";
 
 const Drawer = () => {
 	/* testing */
 	const dispatch = useDispatch();
-	const test = useCallback(() => dispatch(showModal(DiaryModal)), [dispatch]);
+	const test = useCallback(() => dispatch(showModal(CodeModal)), [dispatch]);
 
 	useEffect(() => {
 		console.log("test");
