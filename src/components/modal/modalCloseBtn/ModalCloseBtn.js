880f3a31f0f40a9305d67ecd9ad408baffa2ba44
CutyApple <dbtldhs0915@naver.com>
2020-11-07 16:28:28 +0900

    [FEAT] <components/ModalCloseBtn.js>  ModalCloseBtn 기능 추가

--- src/components/modal/modalCloseBtn/ModalCloseBtn.js
+++ src/components/modal/modalCloseBtn/ModalCloseBtn.js
@@ -1,9 +1,13 @@
 import React from "react";
 import * as S from "./styles";
 import modalCloseBtn from "../../../asset/index";
+import { useDispatch } from "react-redux";
 
 const ModalCloseBtn = () => {
-	return <S.Container img={modalCloseBtn}></S.Container>;
+	const dispatch = useDispatch();
+	const ModalOff = useCallback(() => dispatch(dropModal()), [dispatch]);
+
+	return <S.Container onClick={ModalOff} img={modalCloseBtn}></S.Container>;
 };
 
 export default ModalCloseBtn;
