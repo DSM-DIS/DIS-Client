4e8c20f8de30cf65ac8ff7d46900ce70c168e517
CutyApple <dbtldhs0915@naver.com>
2020-11-08 21:50:43 +0900

    [REFACTOR] 파일 이동으로 인한 주소 변경

--- src/containers/Main/FooterContainer/FooterContainer.js
+++ src/containers/Main/FooterContainer/FooterContainer.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Footer from '../../../Components/Footer/Footer';
+import React from "react";
+import Footer from "../../../components/Footer/Footer";
 
 const FooterContainer = () => {
 	return <Footer />;
