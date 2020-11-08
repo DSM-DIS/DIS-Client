a3241f325e860ffbcfa983a0c658c9b2de3b07a0
CutyApple <dbtldhs0915@naver.com>
2020-11-08 16:18:06 +0900

    [FEAT] <components/CodeModalForm.js> error 기능 추가

--- src/components/modal/ModalForm/CodeModalForm/CodeModalForm.js
+++ src/components/modal/ModalForm/CodeModalForm/CodeModalForm.js
@@ -6,13 +6,15 @@ import ModalFormInput from "../ModalFormInput/ModalFormInput";
 import ModalFormLabel from "../ModalFormLabel/ModalFormLabel";
 
 const CodeModalForm = () => {
+	const error = useSelector((state) => state.modal.error);
+
 	return (
 		<ModalForm>
 			<S.FormItemWrap>
-				<ModalFormInput inputType={"text"} placeHolderText={"ex) 3A48YA"} />
+				<ModalFormInput error={error} inputType={"text"} placeHolderText={"ex) 3A48YA"} />
 				<ModalFormBtn text={"확인"} />
 			</S.FormItemWrap>
-			<ModalFormLabel text={"몰라 에러남 ㅅㄱ"} />
+			{error && <ModalFormLabel text={error} />}
 		</ModalForm>
 	);
 };
