0aa67f782e1b0425489478cc48c5f298762aebec
CutyApple <dbtldhs0915@naver.com>
2020-11-08 16:37:09 +0900

    [REFACTOR] <components/Drawer.js> 불필요 테스팅 코드 삭제

--- src/components/main/Drawer/Drawer.js
+++ src/components/main/Drawer/Drawer.js
@@ -1,22 +1,8 @@
-import React, { useCallback, useEffect } from "react";
+import React from "react";
 import * as S from "./styles.js";
 import DiaryWrap from "./DirayWrap/DirayWrap";
-import { useDispatch } from "react-redux";
-import { showModal } from "../../../modules/redux/modal/index.js";
-import CodeModal from "../../modal/Modal/Codemodal/CodeModal";
-import DiaryModal from "../../modal/Modal/DiaryModal/DiaryModal";
 
 const Drawer = () => {
-	/* testing */
-	const dispatch = useDispatch();
-	const test = useCallback(() => dispatch(showModal(CodeModal)), [dispatch]);
-
-	useEffect(() => {
-		console.log("test");
-		test();
-	});
-	/* testing */
-
 	return (
 		<S.MainWrap>
 			<S.MainHeader>
