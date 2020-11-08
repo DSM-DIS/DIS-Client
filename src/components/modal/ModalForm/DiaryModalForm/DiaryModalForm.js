76ab44dadfe354d0dafbfa3086b637ffc98cafde
CutyApple <dbtldhs0915@naver.com>
2020-11-08 16:17:49 +0900

    [FEAT] <components/DiaryModalForm.js> erro 기능 추가

--- src/components/modal/ModalForm/DiaryModalForm/DiaryModalForm.js
+++ src/components/modal/ModalForm/DiaryModalForm/DiaryModalForm.js
@@ -4,15 +4,21 @@ import ModalForm from "../ModalForm";
 import ModalFormBtn from "../ModalFormBtn/ModalFormBtn";
 import ModalFormInput from "../ModalFormInput/ModalFormInput";
 import ModalFormLabel from "../ModalFormLabel/ModalFormLabel";
+import { useDispatch, useSelector } from "react-redux";
 
 const DiaryModalForm = () => {
+	const error = useSelector((state) => state.modal.error);
+
+	console.log("DiaryModalForm : error");
+	console.log(error);
+
 	return (
 		<ModalForm>
 			<S.FormItemWrap>
-				<ModalFormInput inputType={"text"} placeHolderText={"일기장 이름"} />
+				<ModalFormInput error={error} inputType={"text"} placeHolderText={"일기장 이름"} />
 				<ModalFormBtn text={"확인"} />
 			</S.FormItemWrap>
-			<ModalFormLabel text={"몰라 에러남 ㅅㄱ"} />
+			{error && <ModalFormLabel text={error} />}
 		</ModalForm>
 	);
 };
