fe0b16eab6e253594f79091c5452e83f5aa16338
Giljun Bae <bgj0127@gmail.com>
2020-11-10 21:34:05 +0900

    [ADD] <Backward.js> 이미지 삽입 및 뒤로가기

--- src/components/writing/Backward/style.js
+++ src/components/writing/Backward/style.js
@@ -2,8 +2,15 @@ import styled from "styled-components";
 
 export const BackwardBtn = styled.button`
   background: #fff;
-  width: 50px;
-  height: 50px;
+  width: 3.75rem;
+  height: 3.75rem;
   float: left;
   position: fixed;
+  border: none;
+  margin: 2rem;
+  cursor: pointer;
 `;
+
+export const Img = styled.img`
+  width: 3.75rem;
+`
