00ba434d4597f7f627f929308970a8ffe28c11fa
Wanseo <sonwanseo@gmail.com>
2020-11-14 15:15:05 +0900

    [FEAT] <components/Header.js> add modal dispatch func

--- src/components/login/Header/Header.js
+++ src/components/login/Header/Header.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useCallback } from "react";
 import * as S from "./style";
+import { useDispatch } from "react-redux";
+import { showModal } from "../../../modules/redux/modal";
+import RegisterModal from "../../modal/Modal/RegisterModal/RegisterModal";
 
 const Header = () => {
-  return (
-    <S.HeaderWrapper>
-      <S.HeaderLogo>DIS</S.HeaderLogo>
-      <S.HeaderButtonDiv>
-        <S.HeaderLoginButton>sign in</S.HeaderLoginButton>
-        <S.HeaderRegisterButton>sign up</S.HeaderRegisterButton>
-      </S.HeaderButtonDiv>
-    </S.HeaderWrapper>
-  );
+	const dispatch = useDispatch();
+	const modalOn = useCallback((Modal) => dispatch(showModal(Modal)), [dispatch]);
+
+	return (
+		<S.HeaderWrapper>
+			<S.HeaderLogo>DIS</S.HeaderLogo>
+			<S.HeaderButtonDiv>
+				<S.HeaderLoginButton>sign in</S.HeaderLoginButton>
+				<S.HeaderRegisterButton onClick={() => modalOn(RegisterModal)}>sign up</S.HeaderRegisterButton>
+			</S.HeaderButtonDiv>
+		</S.HeaderWrapper>
+	);
 };
 
 export default Header;
