4b47f59ad2f69c8bf96ebd29aa3cd5de9559643d
CutyApple <dbtldhs0915@naver.com>
2020-12-30 10:29:03 +0900

    [FEAT] <saga/index.js> Apply auth feature

--- src/modules/saga/index.js
+++ src/modules/saga/index.js
@@ -1,8 +1,9 @@
 import { all } from "redux-saga/effects";
+import authSaga from "./auth";
 import diaryListSaga from "./diary_list";
 
 function* rootSaga() {
-	yield all([diaryListSaga()]);
+	yield all([diaryListSaga(), authSaga()]);
 }
 
 export default rootSaga;
