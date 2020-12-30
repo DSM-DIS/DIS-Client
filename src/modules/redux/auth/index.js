6feb129865858d2e67657371da4d54b76b8c0c2c
CutyApple <dbtldhs0915@naver.com>
2020-12-30 10:29:20 +0900

    [FEAT] <redux/auth/index.js>

--- src/modules/redux/auth/index.js
+++ src/modules/redux/auth/index.js
@@ -1,6 +1,29 @@
+export const SET_AUTH_INFO_NAME = "auth/SET_AUTH_INFO_NAME";
+
 export const GET_AUTH_INFO_SAGA = "auth/GET_AUTH_INFO_SAGA";
 
-export const getAuthInfoSaga = (payload) => ({
-	type: GET_AUTH_INFO_SAGA,
+export const setAuthInfoName = (payload) => ({
+	type: SET_AUTH_INFO_NAME,
 	payload,
 });
+
+export const getAuthInfoSaga = () => ({
+	type: GET_AUTH_INFO_SAGA,
+});
+
+const initialState = {
+	name: "",
+};
+
+const authReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case SET_AUTH_INFO_NAME: {
+			return { ...state, name: action.payload };
+		}
+		default: {
+			return state;
+		}
+	}
+};
+
+export default authReducer;
