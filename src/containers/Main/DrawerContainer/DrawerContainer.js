9a5388f24627804eedb11aef7c937f5d6c01e490
CutyApple <dbtldhs0915@naver.com>
2020-12-31 08:20:05 +0900

    .

--- src/containers/Main/DrawerContainer/DrawerContainer.js
+++ src/containers/Main/DrawerContainer/DrawerContainer.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Drawer from "../../../components/drawer/Drawer";
+import { getAuthInfoSaga } from "../../../modules/redux/auth";
 import { getDiaryBookListSaga } from "../../../modules/redux/diary_list";
 
 const DrawerContainer = () => {
+	const username = useSelector((state) => state.auth.username);
+
 	const dispatch = useDispatch();
 
-	useEffect(() => {
+	const initAuthName = useCallback(() => {
+		dispatch(getAuthInfoSaga());
+	}, [dispatch]);
+	const initDiaryBookListSaga = useCallback(() => {
 		dispatch(getDiaryBookListSaga());
+	}, [dispatch]);
+
+	useEffect(() => {
+		initAuthName();
+		initDiaryBookListSaga();
 	}, []);
 
-	return <Drawer />;
+	return <Drawer username={username} />;
 };
 
 export default DrawerContainer;
