1c2673d1506d4a338417d73ccaa52d73be822406
CutyApple <dbtldhs0915@naver.com>
2020-12-30 11:28:10 +0900

    [FIX] <saga/auth/index.js>

--- src/modules/saga/auth/index.js
+++ src/modules/saga/auth/index.js
@@ -1,15 +1,25 @@
+import { call, put, takeEvery } from "redux-saga/effects";
 import { AUTH } from "../../../lib/API";
 import { methodType, requestApiWithoutBodyWithToken } from "../../../lib/REQUEST_API";
+import { GET_AUTH_INFO_SAGA, setAuthInfoName } from "../../redux/auth";
 
 function* getAuthInfo() {
 	try {
 		const REQUEST_URL = AUTH.GET_INFO_BY_ACCESS();
-		const METHOD = methodType.GET;
+		const HTTP_METHOD = methodType.GET;
 
-		const res = requestApiWithoutBodyWithToken(METHOD, REQUEST_URL);
+		const res = yield call(requestApiWithoutBodyWithToken, HTTP_METHOD, REQUEST_URL);
+
+		yield put(setAuthInfoName("a"));
 
 		console.log(res);
 	} catch (error) {
 		console.log(error);
 	}
 }
+
+function* authSaga() {
+	yield takeEvery(GET_AUTH_INFO_SAGA, getAuthInfo);
+}
+
+export default authSaga;
