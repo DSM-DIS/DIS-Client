684143669616151d8282ceb8ce3f5882fc43a426
Wanseo <sonwanseo@gmail.com>
2021-01-01 22:02:52 +0900

    [ADD] add new log func

--- src/lib/LOG.js
+++ src/lib/LOG.js
@@ -1,3 +1,15 @@
+export const Login = (accessToken, refreshToken) => {
+	try {
+		window.localStorage.setItem("ACCESS_TOKEN", accessToken);
+		window.localStorage.setItem("REFRESH_TOKEN", refreshToken);
+	} catch (error) {
+		console.log(error);
+	} finally {
+		alert("로그인에 성공하셨습니다.");
+		window.location.href = "/main";
+	}
+};
+
 export const Logout = () => {
 	try {
 		!!window.localStorage.getItem(ACCESS_TOKEN) && window.localStorage.removeItem(ACCESS_TOKEN);
@@ -8,4 +20,9 @@ export const Logout = () => {
 		alert("로그아웃 되었습니다.");
 		window.location.href = "/";
 	}
+};
+
+export const Register = () => {
+	alert("회원가입에 성공하셨습니다.");
+	window.location.reload();
 };
